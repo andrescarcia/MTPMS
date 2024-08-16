f06aa0d70516f3a641d629bab093a73bc075d528
Andre Scarcia <andrescarciag@gmail.com>
2024-08-16 15:53:01 -0400

    First commit :+1:

---
+++ app/Models/Event.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'case_number',
+        'date',
+        'pais',
+        'CC',
+        'part_number',
+        'description',
+        'quantity',
+        'provider',
+        'unitary_price',
+        'total_price',
+        'priority',
+        'OC',
+        'ETA',
+        'client_id',
+    ];
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+}
---
+++ database/migrations/2024_08_16_193412_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('case_number');
+            $table->date('date');
+            $table->string('pais');
+            $table->string('CC');
+            $table->string('part_number');
+            $table->string('description');
+            $table->integer('quantity');
+            $table->string('provider');
+            $table->decimal('unitary_price');
+            $table->decimal('total_price');
+            $table->string('priority');
+            $table->string('OC');
+            $table->string('ETA');
+            $table->foreignId('client_id')->constrained('client')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
